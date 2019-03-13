8658108dc07e78f10bfe61381dc9d101344a76ee
Shooterek <plokarzbartlomiej@gmail.com>
2019-03-13 10:17:43 +0100

    Add EntityFramework context.

---
+++ Pharmacy.Infrastructure/EF/PharmacyContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Pharmacy.Core.Models;
+
+namespace Pharmacy.Infrastructure.EF
+{
+    public class PharmacyContext : DbContext
+    {
+        private readonly SqlSettings _settings;
+        public DbSet<User> Users { get; set; }
+
+        public PharmacyContext(DbContextOptions<PharmacyContext> options, SqlSettings settings) : base(options)
+        {
+            _settings = settings;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (_settings.InMemory)
+            {
+                optionsBuilder.UseInMemoryDatabase(_settings.InMemoryDatabaseName);
+
+                return;
+            }
+            optionsBuilder.UseSqlServer(_settings.ConnectionString);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            var itemBuilder = modelBuilder.Entity<User>();
+            itemBuilder.HasKey(x => x.Id);
+        }
+    }
+}
---
+++ Pharmacy.Infrastructure/EF/SqlSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace Pharmacy.Infrastructure.EF
+{
+    public class SqlSettings
+    {
+        public string ConnectionString { get; set; }
+        public bool InMemory { get; set; }
+        public string InMemoryDatabaseName { get; set; }
+    }
+}
