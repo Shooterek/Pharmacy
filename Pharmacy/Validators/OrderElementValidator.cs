a24fb0af96a87d0448281871c2603138d6f20a29
Shooterek <plokarzbartlomiej@gmail.com>
2019-04-11 14:34:50 +0200

    Add FluentValidation + Validators

---
+++ Pharmacy/Validators/OrderElementValidator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FluentValidation;
+using Pharmacy.Infrastructure.DTO;
+
+namespace Pharmacy.Validators
+{
+    public class OrderElementValidator : AbstractValidator<OrderElementDto>
+    {
+        public OrderElementValidator()
+        {
+            RuleFor(oe => oe.Quantity).GreaterThan(0);
+            RuleFor(oe => oe.MedicamentId).NotEmpty();
+        }
+    }
+}
---
+++ Pharmacy/Validators/PrescriptionElementValidator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FluentValidation;
+using Pharmacy.Infrastructure.DTO;
+
+namespace Pharmacy.Validators
+{
+    public class PrescriptionElementValidator : AbstractValidator<PrescriptionElementDto>
+    {
+        public PrescriptionElementValidator()
+        {
+            RuleFor(pe => pe.Quantity).GreaterThan(0);
+            RuleFor(pe => pe.MedicamentId).NotEmpty();
+        }
+    }
+}
