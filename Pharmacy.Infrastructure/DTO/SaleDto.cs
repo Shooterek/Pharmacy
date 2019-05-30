519a1de1cdc01af55f02deada8043ac483ed60f3
Shooterek <plokarzbartlomiej@gmail.com>
2019-05-30 22:22:55 +0200

    Set DateOfIssue property

--- Pharmacy.Infrastructure/DTO/SaleDto.cs
+++ Pharmacy.Infrastructure/DTO/SaleDto.cs
@@ -6,15 +6,19 @@ namespace Pharmacy.Infrastructure.DTO
 {
     public class SaleDto
     {
+        public SaleDto()
+        {
+            DateOfIssue = DateTime.UtcNow;
+            Prescriptions = new List<PrescriptionDto>();
+            MedicamentsSoldWithoutPrescription = new List<SaleElementDto>();
+        }
+
         public Guid Id { get; set; }
 
         public Guid PharmacistId { get; set; }
         public UserDto Pharmacist { get; set; }
-
         public DateTime DateOfIssue { get; set; }
-
         public ICollection<PrescriptionDto> Prescriptions { get; set; }
-
         public ICollection<SaleElementDto> MedicamentsSoldWithoutPrescription { get; set; }
     }
 }
